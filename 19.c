830ad98940207e41bcdb86d9761808a29fd408ab
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2022-08-24 23:01:58 +0600

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+int main() 
+{
+    int i,j,k,x=0;
+    for(i=0;i<6;i++){
+        for(j=0;j<i;j++){
+            switch(i+j-1){
+             case -1:  
+             case 0:
+                x += 1;
+                break;
+             case 1:  
+             case 2:   
+             case 3:  
+                x += 2;
+             default:
+                x += 3;
+            }
+            printf("%d\n",x);
+        }
+    }
+    printf("x = %d\n",x);
+    return 0;
+} 
