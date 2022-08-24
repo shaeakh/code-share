42b41639285f4e9d9b6d2a45713cc68eb35dbb2e
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2022-08-24 23:09:51 +0600

    Update 19.c

--- 19.c
+++ 19.c
@@ -8,15 +8,18 @@ int main()
              case -1:  
              case 0:
                 x += 1;
+                printf("%d %d case 0 %d\n",i,j,x);
                 break;
              case 1:  
              case 2:   
              case 3:  
                 x += 2;
+                printf("%d %d case 3 %d\n",i,j,x);
              default:
                 x += 3;
+                printf("%d %d default %d\n",i,j,x);
             }
-            printf("%d\n",x);
+            printf("final %d\n",x);
         }
     }
     printf("x = %d\n",x);
