cd196cdfea94495af9c46c4741437cbaae4d38c3
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-09-23 00:34:10 +0600

    .

--- template.cpp
+++ template.cpp
@@ -2,18 +2,20 @@
 #pragma GCC target("popcnt")
 using namespace std;
 
-
-#define cpresent(c, x) (find(all(c), x) != c.end())
-#define endl '\n'
+//fast.io
+#define _ ios_base::sync_with_stdio(0);
+#define fastt ios_base::sync_with_stdio(false); cin.tie(0);
 
 //typedef long long int ll;
-typedef  float flt;
+typedef float flt;
 typedef double dbl;
-typedef vector<int >  vi;
-typedef pair < int, int > pii;
-typedef pair < ll, ll > pll;
-typedef map<int, int> mii;
 typedef long long int ll;
+typedef long long int64;
+typedef unsigned long long uint64;
+
+
+
+
 
 //constant
 #define MOD 1000000007
@@ -27,10 +29,32 @@ typedef long long int ll;
 #define tr(c, i) for (auto i = c.begin(); i != c.end(); i++)
 #define all(x) begin(x), end(x) 
 #define iterate_print(s) for(auto &x: s) cout<<x<<" " ; cout<<endl ;
+#define pri(a,s,n) for(int i = s; i < n; i++) cout<<a[i]<<" "; cout << endl
 
-//fast.io
-#define _ ios_base::sync_with_stdio(0);
-#define fastt ios_base::sync_with_stdio(false); cin.tie(0);
+//for loop
+#define f(i,a,b)       for(int i=(a);i<(b);i++)
+#define fd(i,a,b)      for(ll i=(a);i>(b);i--)
+#define f0(i,b) for(int i=0;i<b;i++)
+#define f1(i,b) for(int i=1;i<=b;i++)
+#define fi(i,a,b) for(int i=a;i>=b;i--)
+#define fx(i,a,b,dx) for(int i=a;i<b;i+=dx)
+
+//container
+#define pii             pair <int,int>
+#define pll             pair <ll,ll>
+#define pss             pair <string,string>
+#define vi              vector <int>
+#define vl              vector <ll>
+#define vpi             vector <pi>
+#define vpl             vector <pl>
+#define vs              vector <st>
+#define VVI             vector<VI>
+#define mii             map<int,int>
+#define mll             map<ll,ll>
+#define mss             map<string,string>
+#define mls             map<ll,string>
+#define msl             map<string,ll>
+ 
 
 //stl related
 #define F first
@@ -38,16 +62,15 @@ typedef long long int ll;
 #define mp make_pair
 #define mt make_tuple 
 #define pb push_back
-
 #define rsz resize
 #define sz(x) (int) (x).size()
 #define it_multiset  std::multiset<int>::iterator it;
 #define it_mii std::map<int, int>::iterator it;
 #define present(c, x) (c.find(x) != c.end())
+#define cpresent(c, x) (find(all(c), x) != c.end())
+#define clr(x) x.clear()
 
-
-
-//scanf
+//scanf & printf
 #define si(n)                        scanf("%d",&n)
 #define sii(a,b)                    scanf("%d%d",&a,&b)
 #define siii(a,b,c)                scanf("%d%d%d",&a,&b,&c)
@@ -57,12 +80,26 @@ typedef long long int ll;
 #define sf(n)                       scanf("%lf",&n)
 #define ss(n)                       scanf("%s",n)
 
+#define pf             printf
+#define pfi(n)         printf("%d\n",n)
+#define pfl(n)         printf("%lld\n",n)
+#define pfis(n)        printf("%d ",n)
+#define pfls(n)        printf("%lld ",n)
+#define pfci(n,ans)    printf("Case %lld: %d\n",n,ans)
+#define pfcl(n,ans)    printf("Case %lld: %lld\n",n,ans)
+#define pfcd(n,ans)    printf("Case %lld: %lf\n",n,ans)
+
 //others
 #define br '\n'
 #define file freopen("000.txt","r",stdin);
 #define balsal cout<<"dhukse"<<endl;
+#define mem(a,v) memset(a,v,sizeof(a))
+
+
+
 
 void solve(){  
+    cout<<sizeof(ll);
     
 }
 
@@ -89,20 +126,5 @@ int main(){
     file;
     #endif // ONLINE_JUDGE
 5. 
-6.
-7.
-8.
-9.
-10.
-11.
-12.
-13.
-14.
-15.
-16.
-17.
-18.
-19.
-20.
 
 */
