21af56bd299da9cc8bc4aa03058b0391a3744ec7
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-09-23 09:57:24 +0600

    .

--- template.cpp
+++ template.cpp
@@ -7,21 +7,30 @@ using namespace std;
 #define fastt ios_base::sync_with_stdio(false); cin.tie(0);
 
 //typedef long long int ll;
-typedef float flt;
-typedef double dbl;
-typedef long long int ll;
-typedef long long int64;
-typedef unsigned long long uint64;
+typedef float               flt;
+typedef double              dbl;
+typedef long double         ld;
+typedef long long int       lli;
+typedef long long           ll;
+typedef unsigned long long  ull;
 
-
-
-
+//template
+template< class T > T sqr(T n) { return n*n; }
+template< class T > T gcd(T a, T b) { return (b != 0 ? gcd<T>(b, a%b) : a); }
+template< class T > T lcm(T a, T b) { return (a / gcd<T>(a, b) * b); }
+template< class T > T Max(T a, T b) { return a>b?a:b; }
+template< class T > T Min(T a, T b) { return a<b?a:b; }
+template< class T > T abs(T a) { return a>0?a:-a; }
 
 //constant
 #define MOD 1000000007
 #define pi acos(-1.0)
-#define PI 3.141592653589793
-#define INF 1e9
+#define PI 3.1415926535897932384626433832795028841971
+#define INF 0x3f3f3f3f;
+const ll LNF = 0x3f3f3f3f3f3f3f3f;
+const double EPS = 1e-8;
+const ld     EUL = 2.71828182845904523536;  //20digits
+const ld     PII = 3.14159265358979323846;  //20digits
 #define mx_value 1e19
 
 
@@ -30,6 +39,7 @@ typedef unsigned long long uint64;
 #define all(x) begin(x), end(x) 
 #define iterate_print(s) for(auto &x: s) cout<<x<<" " ; cout<<endl ;
 #define pri(a,s,n) for(int i = s; i < n; i++) cout<<a[i]<<" "; cout << endl
+#define foreach(i,n) for(__typeof((n).begin())i =(n).begin();i!=(n).end();i++)
 
 //for loop
 #define f(i,a,b)       for(int i=(a);i<(b);i++)
@@ -38,23 +48,29 @@ typedef unsigned long long uint64;
 #define f1(i,b) for(int i=1;i<=b;i++)
 #define fi(i,a,b) for(int i=a;i>=b;i--)
 #define fx(i,a,b,dx) for(int i=a;i<b;i+=dx)
+#define fn(i,b) for(int i=0;i<n;i++)
 
 //container
 #define pii             pair <int,int>
 #define pll             pair <ll,ll>
 #define pss             pair <string,string>
+
 #define vi              vector <int>
+#define viIt            vector<int>::iterator
 #define vl              vector <ll>
+#define vlIt            vector<ll>::iterator
 #define vpi             vector <pi>
 #define vpl             vector <pl>
 #define vs              vector <st>
-#define VVI             vector<VI>
+#define vvi             vector<vector<int>>
+#define vvll            vector<vector<ll>>  
+
 #define mii             map<int,int>
 #define mll             map<ll,ll>
 #define mss             map<string,string>
 #define mls             map<ll,string>
 #define msl             map<string,ll>
- 
+#define siIt            set<int>::iterator
 
 //stl related
 #define F first
@@ -91,16 +107,16 @@ typedef unsigned long long uint64;
 
 //others
 #define br '\n'
-#define file freopen("000.txt","r",stdin);
-#define balsal cout<<"dhukse"<<endl;
+#define FRO freopen("in.txt","r",stdin);
+#define FRU freopen("out.txt","w",stdout);
 #define mem(a,v) memset(a,v,sizeof(a))
-
-
-
+#define in(n,a,b) ( n>=a && n<=b )
+#define balsal printf("dhukse\n")
+#define yes    printf("YES\n")
+#define no     printf("NO\n")
 
 void solve(){  
-    cout<<sizeof(ll);
-    
+
 }
 
 int main(){
@@ -109,7 +125,7 @@ int main(){
     cin.tie(NULL), cout.tie(NULL);
     
     #ifndef ONLINE_JUDGE
-    file;
+    FRO;
     #endif // ONLINE_JUDGE
 
     solve();
@@ -125,6 +141,14 @@ int main(){
 4. #ifndef ONLINE_JUDGE
     file;
     #endif // ONLINE_JUDGE
-5. 
+5.  #ifndef ONLINE_JUDGE
+    #include "bits/debugTemplate.h"
+    #define dbg(x...)                 \
+        cerr << "[" << #x << "] = ["; \
+        _print(x)
+    #else
+    #define dbg(x...)
+    #endif
+6. 
 
 */
